a0ef6181eb3c0da038fe8461f460c931cfea2bb7
Fernanda Coutinho <fernandarcoutinho@gmail.com>
2023-07-09 21:46:15 -0300

    add dark mode

--- Js/header.js
+++ Js/header.js
@@ -7,3 +7,19 @@ logo_farm.src = "./imgs/Logo_CC.png";
 logo_farm.alt = "logo corporação capsula"; 
 logo_farm.width = 400;
 header.appendChild(logo_farm);
+
+let botao_dark = document.createElement("button");
+let mainHtml = document.getElementsByTagName("html") [0];
+botao_dark.className = "btn";
+botao_dark.innerText = "Modo Escuro";
+botao_dark.addEventListener('click', () => {
+    if(botao_dark.innerText === 'Modo Claro'){
+        mainHtml.setAttribute("data-bs-theme", "light");
+        botao_dark.innerText = 'Modo Escuro';
+    } else {
+        mainHtml.setAttribute("data-bs-theme", "dark");
+        botao_dark.innerText = 'Modo Claro';
+    }
+})
+
+header.appendChild(botao_dark);
